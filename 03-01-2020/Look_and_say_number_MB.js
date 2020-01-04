75cf7b389c4816876cf40891baf1ca547b6582fd
ModestasBar <odekst@gmail.com>
2020-01-04 04:09:00 -0600

    codewars

--- 03-01-2020/Look_and_say_number_MB.js
+++ 03-01-2020/Look_and_say_number_MB.js
@@ -1,14 +1,23 @@
-const generateList = (arr) => {
-    let list = [];
-    let temp = 0;
-    const numGroups = findNumbGroups(arr);
-    for(let i = 0; i < numGroups.length; i++){
-        list.push(numGroups[i] + arr[temp]);
-        temp += numGroups[i];
-    }
-    return list.join('');
-}
-
+/* 6 kyu Look and say numbers
+ Starting with "1" the following lines are produced by 
+"saying what you see", so that line two is "one one", 
+line three is "two one(s)", line four is "one two one one".
+Write a function that given a starting value as a string, 
+returns the appropriate sequence as a list. 
+The starting value can have any number of digits. 
+The termination condition is a defined by the maximum number
+ of iterations, also supplied as an argument.
+*/
+/*
+         1
+         11
+         21
+        1211
+       111221
+       312211
+      13112221
+     1113213211
+*/
 const findNumbGroups = (num) => {
     let scores = [];
     for(let i = 0; i < num.length; i++) {
@@ -26,6 +35,17 @@ const findNumbGroups = (num) => {
     return scores;
 }
 
+const generateList = (arr) => {
+    let list = [];
+    let temp = 0;
+    const numGroups = findNumbGroups(arr);
+    for(let i = 0; i < numGroups.length; i++){
+        list.push(numGroups[i] + arr[temp]);
+        temp += numGroups[i];
+    }
+    return list.join('');
+}
+
 function lookAndSay(data,len){
     let finalList = [];
     let tempData = generateList(data);
@@ -35,46 +55,6 @@ function lookAndSay(data,len){
         len--;
     }
     return finalList;
-  }
-console.log(lookAndSay('1259', 5));
-
-// console.log(generateList('1259'));
-
-const valInterpreter = (val) => {
-    let num;  
-    switch(val) {
-        case '1':
-            num = 'one';
-            break;
-        case '2':
-            num = 'two';
-        break;
-        case '3':
-            num = 'three';
-            break;
-        case '4':
-            num = 'four';
-        break;
-        case '5':
-            num = 'five';
-            break;
-        case '6':
-            num = 'six';
-        break;
-        case '7':
-            num = 'seven';
-            break;
-        case '8':
-            num = 'eight';
-        break;
-        case '9':
-            num = 'nine';
-            break;
-        default:
-            num = 'Invalid value'
-        break;
-    }
-    return num;
 }
 
-// console.log(valInterpreter('9'));
+console.log(lookAndSay('124455', 5));
